8daf600f39c119991b4b4dc5d3c5d09b20321e72
anacarol-costa <carol.costa2502@gmail.com>
2022-02-24 20:13:20 -0300

    criar componente formulario login

---
+++ src/components/login/FormLogin.jsx
@@ -0,0 +1,52 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+
+
+export function FormularioLogin(props) {
+
+    const [formulario, setFormulario] = useState({usuario: "", senha: ""});  
+    
+    
+    const enviarLogin = (event) => {
+        console.log(formulario)
+    }
+    
+
+    return (
+        <Box
+        component="form"
+        sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        >
+        <div>
+            <TextField
+                error
+                id="login-usuario"
+                label="Usuário"          
+                variant="filled"
+                value={formulario.usuario}
+                onChange={(e) => setFormulario({...formulario, usuario: e.target.value })}
+            />
+            <TextField
+                error
+                id="login-senha"
+                label="Senha"          
+                helperText="Incorrect entry."
+                variant="filled"
+                value={formulario.senha}
+                name="senha"
+                onChange={(e) => setFormulario({...formulario, senha: e.target.value })}
+            />
+            </div>
+            <Button variant="contained" onClick={enviarLogin}>Contained</Button>
+            <Outlet />
+        </Box>
+    )
+
+}
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,53 +1,24 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FormularioLogin } from '../components/login/FormLogin';
 
 
 export default function LoginPage () {
  
-    const [formulario, setFormulario] = useState({usuario: "", senha: ""});  
+  const [formulario, setFormulario] = useState({usuario: "", senha: ""});  
  
-
-  const enviarLogin = (event) => {
-    console.log(formulario)
+  
+  const logarComGoogle = () => {
+    console.log("google")
   }
 
 
 
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-          <TextField
-            error
-            id="login-usuario"
-            label="Usuário"          
-            variant="filled"
-            value={formulario.usuario}
-            onChange={(e) => setFormulario({...formulario, usuario: e.target.value })}
-          />
-          <TextField
-            error
-            id="login-senha"
-            label="Senha"          
-            helperText="Incorrect entry."
-            variant="filled"
-            value={formulario.senha}
-            name="senha"
-            onChange={(e) => setFormulario({...formulario, senha: e.target.value })}
-          />
-          </div>
-          <Button variant="contained" onClick={enviarLogin}>Contained</Button>
-          <Outlet />
-      </Box>
+      <>
+        <FormularioLogin />
+        <Button variant="contained" onClick={logarComGoogle}>Entre com Google</Button>
+      </>
     )   
 }
 
