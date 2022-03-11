e9ca3268a7bf22017e4b28311cb65b667c006444
Ana Carolina da Costa <carol.costa2502@gmail.com>
2022-03-11 20:53:47 -0300

    implementar validacao dos inputs de formUsuario com heact hook form, MUI e yup

--- src/pages/FormUsuario.jsx
+++ src/pages/FormUsuario.jsx
@@ -1,7 +1,10 @@
-import { Button, FormControl, FormHelperText, Input, InputLabel, TextField } from '@mui/material';
-import axios from 'axios';
+import { Button, TextField, Typography } from '@mui/material';
 import React, { Component, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Outlet, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import axios from 'axios';
 
 
 export default function FormUsuario() {
@@ -9,15 +12,48 @@ export default function FormUsuario() {
   const [cadastro, setCadastro] = useState({ nome: "", email: "", senha: "", repetirSenha: "", telefone: "" });
   const [errorSenhasDiferentes, setErrorSenhasDiferentes] = useState('');
 
+  const validacaoSchema = Yup.object().shape({
+    nome: Yup.string()
+      .required('campo obrigatório')
+      .max(20, 'campo excedeu o limite máximo de 20 caracteres'),
+
+    email: Yup.string()
+      .required('email obrigatório')
+      .email('email inválido'),
+
+    senha: Yup.string()
+      .required('campo obrigatório')
+      .min(6, 'senha deve possuir no mínimo 6 caracteres')
+      .max(40, 'senha excedeu o limite máximo de 40 caracteres'),
+
+    repetirSenha: Yup.string()
+      .required('confirme a senha')
+      .oneOf([Yup.ref('senha'), null], 'senha incompatível'),
+
+    telefone: Yup.string()
+      .required('campo obrigatório')
+      .min(11),
+  })
+
+  const {
+    register,
+    //control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(validacaoSchema)
+  });
+
   const enviarCadastro = async (event) => {
-    const newUsuario = { ...cadastro };
+    console.log(event);
+    const newUsuario = { ...event };
     delete newUsuario.repetirSenha;
     await axios.post("https://sigen-backend.herokuapp.com/usuarios", { ...newUsuario });
   }
 
   const verificarSenhas = () => {
     if (cadastro.repetirSenha && cadastro.repetirSenha !== cadastro.senha) {
-      setErrorSenhasDiferentes("Senhas diferentes");
+      setErrorSenhasDiferentes("Senhas diferentes")
     } else {
       setErrorSenhasDiferentes("");
     }
@@ -40,13 +76,18 @@ export default function FormUsuario() {
     >
       <div>
         <TextField
+          required
           id="nome-usuario"
           label="Nome"
           type="string"
           autoComplete="current-string"
           variant="standard"
-          onChange={(e) => setCadastro({ ...cadastro, nome: e.target.value })}
+          {...register('nome')}
+          error={errors.nome ? true : false}
         />
+        <Typography variant="inherit" color="#d32f2f">
+          {errors.nome?.message}
+        </Typography>
       </div>
 
       <div>
@@ -56,8 +97,12 @@ export default function FormUsuario() {
           type="string"
           autoComplete="current-string"
           variant="standard"
-          onChange={(e) => setCadastro({ ...cadastro, email: e.target.value })}
+          {...register('email')}
+          error={errors.email ? true : false}
         />
+        <Typography variant="inherit" color="#d32f2f">
+          {errors.email?.message}
+        </Typography>
       </div>
 
       <div>
@@ -67,8 +112,12 @@ export default function FormUsuario() {
           type="password"
           autoComplete="current-password"
           variant="standard"
-          onChange={(e) => setCadastro({ ...cadastro, senha: e.target.value })}
+          {...register('senha')}
+          error={errors.senha ? true : false}
         />
+        <Typography variant="inherit" color="#d32f2f">
+          {errors.senha?.message}
+        </Typography>
       </div>
 
       <div>
@@ -80,8 +129,12 @@ export default function FormUsuario() {
           autoComplete="current-password"
           variant="standard"
           onBlur={verificarSenhas}
-          onChange={(e) => setCadastro({ ...cadastro, repetirSenha: e.target.value })}
+          {...register('repetirSenha')}
+          error={errors.repetirSenha ? true : false}
         />
+        <Typography variant="inherit" color="#d32f2f">
+          {errors.repetirSenha?.message}
+        </Typography>
       </div>
 
       <div>
@@ -92,14 +145,15 @@ export default function FormUsuario() {
           type="tel"
           autoComplete="current-tel"
           variant="standard"
-          onChange={(e) => setCadastro({ ...cadastro, telefone: e.target.value })}
+          {...register('telefone')}
+          error={errors.telefone ? true : false}
         />
-
-
-        {/* to do: arrumar o inputprops */}
-
+        <Typography variant="inherit" color="#d32f2f">
+          {errors.telefone?.message}
+        </Typography>
       </div>
-      <Button variant='contained' onClick={enviarCadastro}>Enviar</Button>
+
+      <Button variant='contained' onClick={handleSubmit(enviarCadastro)}>Enviar</Button>
       <Outlet />
     </ form >
 
