99d995b992cbf612061458974fa24f764cb493e0
anacarol-costa <carol.costa2502@gmail.com>
2022-03-23 20:49:02 -0300

    ajustar paramentro tokenUsuario

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import {
     Link, useNavigate
 } from "react-router-dom";
 import Header from './components/shared/Header';
+import sessionUtil from './util/sessionUtil';
 
 export default function App() {
+
+  const tokenUsuario = sessionUtil.getTokenCookieDecode();
 
   return (
     <div className="App" >
