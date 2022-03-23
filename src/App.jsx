67b8862f564debe8f1f107a5f407cbb6895cb4c8
anacarol-costa <carol.costa2502@gmail.com>
2022-03-23 20:45:41 -0300

    remover validacao de usuario nao autenticado

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,8 @@ import {
     Link, useNavigate
 } from "react-router-dom";
 import Header from './components/shared/Header';
-import sessionUtil from './util/sessionUtil';
-import {useEffect} from "react";
 
 export default function App() {
-    const navigate = useNavigate();
-    let tokenUsuario = {};
-
-    useEffect(() => {
-        const tokenJwt = sessionUtil.getToken();
-        if(!tokenJwt) {
-            navigate('/')
-        } else {
-            tokenUsuario = sessionUtil.getTokenCookieDecode();
-        }
-    })
 
   return (
     <div className="App" >
