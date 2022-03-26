b50433f9e26a73fb7ad4ca4cc4d5741991898fcd
anacarol-costa <carol.costa2502@gmail.com>
2022-03-26 12:21:13 -0300

    atualizar data grid depois de delecai

--- src/pages/usuario/ListaUsuario.jsx
+++ src/pages/usuario/ListaUsuario.jsx
@@ -7,39 +7,42 @@ import {Button} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 
-const columns = [
-    { field: 'nome', headerName: 'Nome', width: 130 },
-    { field: 'telefone', headerName: 'Telefone', width: 130 },
-    { field: 'email', headerName: 'E-mail', width: 130 },
-
-    {
-        field: 'actions',
-        type: 'actions',
-        width: 80,
-        getActions: (params) => [
-            <GridActionsCellItem
-                icon={<DeleteIcon />}
-                label="Delete"
-                onClick={()=> delatarUsuario(params.id)}
-            />,
-        ]
-    }
-]
-
-const delatarUsuario = async (id) => {
-    await axiosSemAutorizacao.delete(`/usuarios/${id}`);
-};
-
 export default function ListaUsuario() {
     const navigate = useNavigate();
     const [rows, setRows] = useState([]);
+    const columns = [
+        { field: 'nome', headerName: 'Nome', width: 130 },
+        { field: 'telefone', headerName: 'Telefone', width: 130 },
+        { field: 'email', headerName: 'E-mail', width: 130 },
 
+        {
+            field: 'actions',
+            type: 'actions',
+            width: 80,
+            getActions: (params) => [
+                <GridActionsCellItem
+                    icon={<DeleteIcon />}
+                    label="Delete"
+                    onClick={()=> delatarUsuario(params.id)}
+                />,
+            ]
+        }
+    ]
+
+    const delatarUsuario = async (id) => {
+        await axiosSemAutorizacao.delete(`/usuarios/${id}`);
+        await consultarUsuario();
+    };
 
 
     useEffect(async () =>{
+        await consultarUsuario();
+    }, [])
+
+    const consultarUsuario = async () => {
         const {data} = await axiosSemAutorizacao.get('/usuarios');
         setRows(data);
-    }, [])
+    }
 
     const novoUsuario = () => {
         navigate('/private/administracao/novo-usuario')
