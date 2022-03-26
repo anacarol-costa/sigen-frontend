78e09606e6876420dd57790e67e1e9226ab1713e
anacarol-costa <carol.costa2502@gmail.com>
2022-03-26 11:45:33 -0300

    refatorar fetch usuarios

--- src/pages/usuario/ListaUsuario.jsx
+++ src/pages/usuario/ListaUsuario.jsx
@@ -19,14 +19,9 @@ const columns = [
 export default function ListaUsuario() {
     const [rows, setRows] = useState([]);
     useEffect(async () =>{
-        const usuarios = await obterUsuarios();
-        setRows(usuarios);
-    }, [])
-
-    const obterUsuarios = async () => {
         const {data} = await axiosSemAutorizacao.get('/usuarios');
-        return data;
-    }
+        setRows(data);
+    }, [])
 
     return (
         <div style={{ height: 400, width: '100%' }}>
