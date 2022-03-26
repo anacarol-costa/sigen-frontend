6d9aec783f1f95eda6053939ecb40991a7f15f47
anacarol-costa <carol.costa2502@gmail.com>
2022-03-26 12:17:42 -0300

    deletar usuario

--- src/pages/usuario/ListaUsuario.jsx
+++ src/pages/usuario/ListaUsuario.jsx
@@ -1,26 +1,39 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
 import axiosSemAutorizacao from "../../util/axios/axiosSemAutorizacao";
 import {useEffect, useState} from "react";
 import {Button} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
-function getFullName(params) {
-    return `${params.row.firstName || ''} ${params.row.lastName || ''}`;
-}
 
 const columns = [
     { field: 'nome', headerName: 'Nome', width: 130 },
     { field: 'telefone', headerName: 'Telefone', width: 130 },
     { field: 'email', headerName: 'E-mail', width: 130 },
 
-];
+    {
+        field: 'actions',
+        type: 'actions',
+        width: 80,
+        getActions: (params) => [
+            <GridActionsCellItem
+                icon={<DeleteIcon />}
+                label="Delete"
+                onClick={()=> delatarUsuario(params.id)}
+            />,
+        ]
+    }
+]
 
-
+const delatarUsuario = async (id) => {
+    await axiosSemAutorizacao.delete(`/usuarios/${id}`);
+};
 
 export default function ListaUsuario() {
     const navigate = useNavigate();
     const [rows, setRows] = useState([]);
+
 
 
     useEffect(async () =>{
