ad9457466d50d59d10ecae72b4ff424a363a93d3
unknown <carol.costa2502@gmail.com>
2022-03-29 19:02:18 -0300

    adicionar snackbar de mensagem sucesso de exclusão de usuario no componente lista usuario

--- src/pages/usuario/ListaUsuario.jsx
+++ src/pages/usuario/ListaUsuario.jsx
@@ -2,45 +2,52 @@ import * as React from 'react';
 import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import axiosSemAutorizacao from "../../util/axios/axiosSemAutorizacao";
-import {useEffect, useState} from "react";
-import {Button} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { mostrarMensagemSucesso } from '../../store/snackbar-reducer';
+import { useDispatch } from 'react-redux';
 
 
 export default function ListaUsuario() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [rows, setRows] = useState([]);
     const columns = [
         { field: 'nome', headerName: 'Nome', width: 130 },
         { field: 'telefone', headerName: 'Telefone', width: 130 },
-        { field: 'email', headerName: 'E-mail', width: 130 },
+        { field: 'email', headerName: 'E-mail', width: 200 },
 
         {
             field: 'actions',
             type: 'actions',
-            width: 80,
+            headerName: 'Deletar usuário',
+            width: 120,
             getActions: (params) => [
                 <GridActionsCellItem
                     icon={<DeleteIcon />}
                     label="Delete"
-                    onClick={()=> delatarUsuario(params.id)}
+                    onClick={() => delatarUsuario(params.id)}
                 />,
             ]
         }
     ]
 
     const delatarUsuario = async (id) => {
+        console.log('deletar usuario');
         await axiosSemAutorizacao.delete(`/usuarios/${id}`);
         await consultarUsuario();
+        dispatch(mostrarMensagemSucesso('Usuário excluído com sucesso.'));
+        navigate('/private/administracao/usuarios');
     };
 
 
-    useEffect(async () =>{
+    useEffect(async () => {
         await consultarUsuario();
     }, [])
 
     const consultarUsuario = async () => {
-        const {data} = await axiosSemAutorizacao.get('/usuarios');
+        const { data } = await axiosSemAutorizacao.get('/usuarios');
         setRows(data);
     }
 
