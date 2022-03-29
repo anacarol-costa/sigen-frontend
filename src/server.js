53a3d683af4d8a448e5aa5296229a74fe8d2ba1a
anacarol-costa <carol.costa2502@gmail.com>
2022-03-29 12:51:49 -0300

    adicionar Procfile para heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 // Arquivo para execucao em prod
 const express = require('express');
-const { resolve } = require('path')
+const { resolve } = require('path');
 
 
 const app = express();
 
-app.use('/', express.static(resolve(__dirname,'./build')));
+app.use('/',
+    express.static(
+        resolve(
+            __dirname,'./build'
+        )
+    )
+);
 
 app.listen(process.env.PORT || 3000, (error) => {
     if (error) {
