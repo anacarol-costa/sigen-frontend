4b7bbddc36a9900295603194c608f56d7f4f02dc
anacarol-costa <carol.costa2502@gmail.com>
2022-03-29 13:32:51 -0300

    ajustar arquivos estaticos de server express

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
-const path = require('path');
+// Arquivo para execucao em prod
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'./build/index.html'));
 });
-app.listen(port, () => {
-    console.log('Server is up!');
-});
+
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log('Error up server', error)
+    }
+
+    console.log('App is listening on port' + PORT);
+})
