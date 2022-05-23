2514a72751b02f30245459da9a95d9235c532593
unknown <carol.costa2502@gmail.com>
2022-05-23 16:27:37 -0300

    inserir img 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,22 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import imagemErro404 from "../img/imagemErro404.png"
+
 export default function NotFoundPage() {
 
 
-    return(
-        <h2>Página não encontrada.</h2>
+    return (
+        <Box>
+            <h2>Página não encontrada!</h2>
+            <Grid img xs={'50%'}>
+                <img
+                    src={imagemErro404}
+                    alt='imagem erro 404 de página não encontrada'
+                    style={{ height: '100%', width: "100%" }}
+                />
+            </Grid>
+        </Box>
+
     )
 
 }
