7c7c5c239df096b0bb65226d29053423ecbfcf19
unknown <carol.costa2502@gmail.com>
2022-05-23 17:50:14 -0300

    acrescentar informações de data de entrega e horario de entrega da encomenda

--- src/pages/encomenda/ResumoEncomendaPage.jsx
+++ src/pages/encomenda/ResumoEncomendaPage.jsx
@@ -1,8 +1,8 @@
-import {Box} from "@mui/system";
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { Box } from "@mui/system";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axiosComAutorizacao from "../../util/axios/axiosComAutorizacao";
-import {Divider, Grid, Typography} from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
 import encomendaUtil from "../../util/encomenda/encomendaUtil";
 
 export default function ResumoEncomendaPage() {
@@ -16,14 +16,14 @@ export default function ResumoEncomendaPage() {
 
     return (
         <Grid container
-              spacing={0}
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              style={{ minHeight: '100vh' }}
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
         >
             <h1>Resumo da Compra</h1>
-            { compra.enderecoCompra &&
+            {compra.enderecoCompra &&
                 <Box
                     sx={{
                         background: 'white',
@@ -32,48 +32,66 @@ export default function ResumoEncomendaPage() {
                     }}
                     elevation={3}
                 >
-                <Box sx={{ my: 3, mx: 2 }}>
-                    <Grid container alignItems="center">
-                        <Grid item xs>
+                    <Box sx={{ my: 3, mx: 2 }}>
+                        <Grid container alignItems="center">
+                            <Grid item xs>
 
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Box>
-                <Box sx={{ my: 3, mx: 2 }}>
-                    <Box direction="row" spacing={5}>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Produto(s)
-                        </Typography>
-                        <Box>
-                            {compra.produtos.map(produto => (
-                                <Box>
-                                    <Typography gutterBottom variant="body2" component="div">
-                                        {
-                                            Object.entries(encomendaUtil.normalizarItensProduto(produto.itensProduto)).map(([opcao, item]) => (
-                                                <Box key={opcao}>
-                                                    <h4>{opcao}</h4>
+                    </Box>
+                    <Box sx={{ my: 3, mx: 2 }}>
+                        <Box direction="row" spacing={5}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Produto(s)
+                            </Typography>
+                            <Box>
+                                {compra.produtos.map(produto => (
+                                    <Box>
+                                        <Typography gutterBottom variant="body2" component="div">
+                                            {
+                                                Object.entries(encomendaUtil.normalizarItensProduto(produto.itensProduto)).map(([opcao, item]) => (
                                                     <Box key={opcao}>
-                                                        {item.map((item) => (
-                                                            <Box key={item.id}>
-                                                                <Box>{item.nome} - R${item.total || item.valor}</Box>
-                                                                <br/>
-                                                            </Box>
-                                                        ))}
+                                                        <h4>{opcao}</h4>
+                                                        <Box key={opcao}>
+                                                            {item.map((item) => (
+                                                                <Box key={item.id}>
+                                                                    <Box>{item.nome} - R${item.total || item.valor}</Box>
+                                                                    <br />
+                                                                </Box>
+                                                            ))}
+                                                        </Box>
                                                     </Box>
-                                                </Box>
-                                            ))
-                                        }
-                                    </Typography>
-                                    <Divider variant="middle" />
-                                </Box>
-                            ))}
+                                                ))
+                                            }
+                                        </Typography>
+                                        <Divider variant="middle" />
+                                    </Box>
+                                ))}
+                            </Box>
                         </Box>
                     </Box>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Total R${compra.valorCompra}
+                    </Typography>
+                    <Divider variant="middle" />
+                    <Typography gutterBottom variant="h5" component="div">
+                        Data de entrega
+                        <Box>
+                            <Typography>
+                                {compra.dia}.0{compra.mes}.{compra.ano}
+                            </Typography>
+                        </Box>
+                    </Typography>
+                    <Divider variant="middle" />
+                    <Typography gutterBottom variant="h5" component="div">
+                        Horário de entrega
+                        <Box>
+                            <Typography>
+                                {compra.hora}
+                            </Typography>
+                        </Box>
+                    </Typography>
                 </Box>
-                <Typography gutterBottom variant="h5" component="div">
-                    Total R${compra.valorCompra}
-                </Typography>
-            </Box>
             }
         </Grid>
     )
