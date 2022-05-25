dd52a83c9ad2ae47c8025f839ef2bcbc74db1c95
unknown <carol.costa2502@gmail.com>
2022-05-25 15:25:13 -0300

    acrescentar get ao form conta usuario

--- src/components/usuario/FormContaUsuario.jsx
+++ src/components/usuario/FormContaUsuario.jsx
@@ -1,40 +1,33 @@
-import { Box, TextField, InputLabel } from "@mui/material";
+import { Box, TextField, InputLabel, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axiosComAutorizacao from "../../util/axios/axiosComAutorizacao";
 import sessionUtil from "../../util/sessionUtil";
 
 export default function FormContaUsuario(props) {
-    const [form, setForm] = useState([]);
-    const [endereco] = useState({ cep: 'CEP', quadra: 'Quadra', bairro: 'Bairro', complemento: 'Complemento', pontoReferencia: 'Ponto de referência' })
+    const { id } = useParams();
+    const [conta, setConta] = useState({});
 
     useEffect(async () => {
-        await recuperarDadosUsuario();
+        const { data } = await axiosComAutorizacao.get(`/usuarios/${id}`)                
+        setConta(data);
     }, []);
 
-    const recuperarDadosUsuario = async () => {
-        try {
-            const { data } = await axiosComAutorizacao.get(`/usuarios/${sessionUtil.getIdUsuario()}`);
-            setForm(data);
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
-    const handleObterDadosUsuario = ({ nome, email, senha, telefone, endereco }) => {
-        const dadosUsuario = [...form]
-    }
 
     return (
         <Box sx={{ display: 'inline-grid' }}>
+            <Typography>
+                Nome {conta.nome}
+            </Typography>
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="nome-usuario"
                 label="Nome"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+                {...conta.nome}
             />
             <TextField
                 sx={{ width: '40vw', m: 1 }}
@@ -43,76 +36,75 @@ export default function FormContaUsuario(props) {
                 label="Email"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="senha-usuario"
                 label="Senha"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="telefone-usuario"
                 label="Telefone"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
 
             <InputLabel id="label-endereço" sx={{ paddingTop: '6%' }} align="left">
                 Endereço
             </InputLabel>
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="cep-usuario"
                 label="CEP"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="bairro-usuario"
                 label="Bairro"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="quadra-usuario"
                 label="Quadra"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="complemento-usuario"
                 label="Complemento"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
             <TextField
-                sx={{ width: '40vw', m: 1  }}
+                sx={{ width: '40vw', m: 1 }}
                 // disabled
                 id="ponto-referencia-usuario"
                 label="Ponto de referência"
                 type="string"
                 variant="standard"
-                onLoad={recuperarDadosUsuario}
+            // onLoad={recuperarDadosUsuario}
             />
         </Box>
-
     )
 }
